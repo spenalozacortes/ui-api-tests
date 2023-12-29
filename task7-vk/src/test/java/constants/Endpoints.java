cf054ba6d2e67d78d9c5f160f5a9bcd4807eadf4
Stephanie Peñaloza <spenalozacortes@gmail.com>
2023-12-29 09:12:11 -0600

    Create private constants

--- task7-vk/src/test/java/constants/Endpoints.java
+++ task7-vk/src/test/java/constants/Endpoints.java
@@ -4,11 +4,14 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Endpoints {
-    public static final String CREATE_POST = "/wall.post";
-    public static final String EDIT_POST = "/wall.edit";
-    public static final String DELETE_POST = "/wall.delete";
-    public static final String ADD_COMMENT = "/wall.createComment";
-    public static final String GET_LIKES = "/likes.getList";
-    public static final String GET_UPLOAD_SERVER = "/photos.getWallUploadServer";
-    public static final String SAVE_PHOTO = "/photos.saveWallPhoto";
+    private static final String WALL = "/wall";
+    private static final String LIKES = "/likes";
+    private static final String PHOTOS = "/photos";
+    public static final String CREATE_POST = String.format("%s.post", WALL);
+    public static final String EDIT_POST = String.format("%s.edit", WALL);
+    public static final String DELETE_POST = String.format("%s.delete", WALL);
+    public static final String ADD_COMMENT = String.format("%s.createComment", WALL);
+    public static final String GET_LIKES = String.format("%s.getList", LIKES);
+    public static final String GET_UPLOAD_SERVER = String.format("%s.getWallUploadServer", PHOTOS);
+    public static final String SAVE_PHOTO = String.format("%s.saveWallPhoto", PHOTOS);
 }
