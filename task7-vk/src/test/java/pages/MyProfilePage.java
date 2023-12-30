72b770db1d31d037ec5c7766ec4d408007446064
Stephanie Peñaloza <spenalozacortes@gmail.com>
2023-12-29 18:22:25 -0600

    Parameterize methods in MyProfilePage by postId

--- task7-vk/src/test/java/pages/MyProfilePage.java
+++ task7-vk/src/test/java/pages/MyProfilePage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import aquality.selenium.elements.ElementType;
 import aquality.selenium.elements.interfaces.IButton;
 import aquality.selenium.elements.interfaces.ILabel;
 import aquality.selenium.elements.interfaces.ILink;
@@ -8,43 +9,65 @@ import org.openqa.selenium.By;
 
 public class MyProfilePage extends Form {
 
+    private static final String POST_XPATH = "//div[contains(@id, 'post') and contains(@id, '_%d')]";
+    private static final By POST_TEXT_BY = By.className("wall_post_text");
+    private static final By AUTHOR_BY = By.xpath("//*[contains(@class, 'wall_post_cont')]");
+    private static final By PHOTO_LINK_BY = By.xpath("//a[contains(@class,'image_cover')]");
+    private static final By NEXT_COMMENT_LINK_BY = By.xpath("//a[contains(@class, 'replies_next')]");
+    private static final By REPLY_AUTHOR_LINK_BY = By.xpath("//*[@class='reply_author']//a[contains(@class, 'author')]");
+    private static final By LIKE_BTN_BY = By.xpath("//*[contains(@class,'PostButtonReactions')]");
     private static final String ID = "id";
-    private final IButton likeBtn = getElementFactory().getButton(By.xpath("//*[contains(@class,'PostButtonReactions')]"), "Like button");
-    private final ILabel postText = getElementFactory().getLabel(By.className("wall_post_text"), "Post text");
-    private final ILabel authorLabel = getElementFactory().getLabel(By.xpath("//*[contains(@class, 'wall_post_cont')]"), "Author label");
-    private final ILink photoLink = getElementFactory().getLink(By.xpath("//a[contains(@class,'image_cover')]"), "Image link");
-    private final ILink nextCommentLink = getElementFactory().getLink(By.xpath("//a[contains(@class, 'replies_next')]"), "Show next comment link");
-    private final ILink replyAuthorLink = getElementFactory().getLink(By.xpath("//*[@class='reply_author']//a[contains(@class, 'author')]"), "Reply author link");
+    private ILabel post;
+    private ILabel postText;
+    private ILabel authorLabel;
+    private ILink photoLink;
+    private ILink nextCommentLink;
+    private ILink replyAuthorLink;
+    private IButton likeBtn;
 
     public MyProfilePage() {
         super(By.className("ProfileHeaderButton"), "My Profile page");
     }
 
-    public void clickLikeBtn() {
-        likeBtn.click();
-    }
-
-    public String getPostText() {
+    public String getPostText(int postId) {
+        post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
+        postText = post.findChildElement(POST_TEXT_BY, ElementType.LABEL);
         return postText.getText();
     }
 
-    public String getAuthor() {
+    public String getAuthor(int postId) {
+        post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
+        authorLabel = post.findChildElement(AUTHOR_BY, ElementType.LABEL);
         return authorLabel.getAttribute(ID);
     }
 
-    public String getPhoto() {
+    public String getPhoto(int postId) {
+        post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
+        photoLink = post.findChildElement(PHOTO_LINK_BY, ElementType.LINK);
         return photoLink.getHref();
     }
 
-    public void clickNextCommentLink() {
+    public void clickNextCommentLink(int postId) {
+        post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
+        nextCommentLink = post.findChildElement(NEXT_COMMENT_LINK_BY, ElementType.LINK);
         nextCommentLink.click();
     }
 
-    public String getReplyAuthor() {
+    public String getReplyAuthor(int postId) {
+        post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
+        replyAuthorLink = post.findChildElement(REPLY_AUTHOR_LINK_BY, ElementType.LINK);
         return replyAuthorLink.getHref();
     }
 
-    public boolean isPostDisplayed() {
+    public void clickLikeBtn(int postId) {
+        post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
+        likeBtn = post.findChildElement(LIKE_BTN_BY, ElementType.BUTTON);
+        likeBtn.click();
+    }
+
+    public boolean isPostDisplayed(int postId) {
+        post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
+        postText = post.findChildElement(POST_TEXT_BY, ElementType.LABEL);
         return postText.state().isDisplayed();
     }
 }
--- task7-vk/src/test/java/tests/VkTests.java
+++ task7-vk/src/test/java/tests/VkTests.java
@@ -56,27 +56,27 @@ public class VkTests extends BaseTest {
         PostResponse post = wallSteps.createPost(postMessage);
         int postId = post.getPostId();
         int ownerId = CredentialsConfig.getOwnerId();
-        Assert.assertEquals(myProfilePage.getPostText(), postMessage, "Post text is not as expected");
-        Assert.assertTrue(myProfilePage.getAuthor().contains(String.valueOf(ownerId)), "Post author is incorrect");
+        Assert.assertEquals(myProfilePage.getPostText(postId), postMessage, "Post text is not as expected");
+        Assert.assertTrue(myProfilePage.getAuthor(postId).contains(String.valueOf(ownerId)), "Post author is incorrect");
 
         String editedMessage = RandomUtils.generateRandomString(Constants.POST_LENGTH);
         PhotoResponse savePhoto = photoSteps.saveFile(IMAGE_PATH);
         int photoId = savePhoto.getId();
         String photo = String.format("photo%d_%d", ownerId, photoId);
         wallSteps.editPost(postId, editedMessage, photo);
-        Assert.assertEquals(myProfilePage.getPostText(), editedMessage, "Post text is not updated");
-        Assert.assertTrue(myProfilePage.getPhoto().contains(photo), "Photos are not the same");
+        Assert.assertEquals(myProfilePage.getPostText(postId), editedMessage, "Post text is not updated");
+        Assert.assertTrue(myProfilePage.getPhoto(postId).contains(photo), "Photos are not the same");
 
         String comment = RandomUtils.generateRandomString(Constants.COMMENT_LENGTH);
         wallSteps.addCommentToPost(postId, comment);
-        myProfilePage.clickNextCommentLink();
-        Assert.assertTrue(myProfilePage.getReplyAuthor().contains(String.valueOf(ownerId)), "Comment is from incorrect user");
+        myProfilePage.clickNextCommentLink(postId);
+        Assert.assertTrue(myProfilePage.getReplyAuthor(postId).contains(String.valueOf(ownerId)), "Comment is from incorrect user");
 
-        myProfilePage.clickLikeBtn();
+        myProfilePage.clickLikeBtn(postId);
         LikeResponse isLiked = wallSteps.getIsLiked(postId, ownerId);
         Assert.assertEquals(isLiked.getLiked(), Constants.LIKED, "No like from expected user");
 
         wallSteps.deletePost(postId);
-        Assert.assertFalse(myProfilePage.isPostDisplayed(), "Post is not deleted");
+        Assert.assertFalse(myProfilePage.isPostDisplayed(postId), "Post is not deleted");
     }
 }
