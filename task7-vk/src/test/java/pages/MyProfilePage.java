566bd0452ccd519365511fb078da89d6c785ac21
Stephanie Peñaloza <spenalozacortes@gmail.com>
2024-01-01 13:13:46 -0600

    Make variables local

--- task7-vk/src/test/java/pages/MyProfilePage.java
+++ task7-vk/src/test/java/pages/MyProfilePage.java
@@ -17,57 +17,50 @@ public class MyProfilePage extends Form {
     private static final By REPLY_AUTHOR_LINK_BY = By.xpath("//*[@class='reply_author']//a[contains(@class, 'author')]");
     private static final By LIKE_BTN_BY = By.xpath("//*[contains(@class,'PostButtonReactions')]");
     private static final String ID = "id";
-    private ILabel post;
-    private ILabel postText;
-    private ILabel authorLabel;
-    private ILink photoLink;
-    private ILink nextCommentLink;
-    private ILink replyAuthorLink;
-    private IButton likeBtn;
 
     public MyProfilePage() {
         super(By.className("ProfileHeaderButton"), "My Profile page");
     }
 
     public String getPostText(int postId) {
-        post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
-        postText = post.findChildElement(POST_TEXT_BY, ElementType.LABEL);
+        ILabel post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
+        ILabel postText = post.findChildElement(POST_TEXT_BY, ElementType.LABEL);
         return postText.getText();
     }
 
     public String getAuthor(int postId) {
-        post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
-        authorLabel = post.findChildElement(AUTHOR_BY, ElementType.LABEL);
+        ILabel post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
+        ILabel authorLabel = post.findChildElement(AUTHOR_BY, ElementType.LABEL);
         return authorLabel.getAttribute(ID);
     }
 
     public String getPhoto(int postId) {
-        post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
-        photoLink = post.findChildElement(PHOTO_LINK_BY, ElementType.LINK);
+        ILabel post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
+        ILink photoLink = post.findChildElement(PHOTO_LINK_BY, ElementType.LINK);
         return photoLink.getHref();
     }
 
     public void clickNextCommentLink(int postId) {
-        post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
-        nextCommentLink = post.findChildElement(NEXT_COMMENT_LINK_BY, ElementType.LINK);
+        ILabel post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
+        ILink nextCommentLink = post.findChildElement(NEXT_COMMENT_LINK_BY, ElementType.LINK);
         nextCommentLink.click();
     }
 
     public String getReplyAuthor(int postId) {
-        post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
-        replyAuthorLink = post.findChildElement(REPLY_AUTHOR_LINK_BY, ElementType.LINK);
+        ILabel post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
+        ILink replyAuthorLink = post.findChildElement(REPLY_AUTHOR_LINK_BY, ElementType.LINK);
         return replyAuthorLink.getHref();
     }
 
     public void clickLikeBtn(int postId) {
-        post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
-        likeBtn = post.findChildElement(LIKE_BTN_BY, ElementType.BUTTON);
+        ILabel post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
+        IButton likeBtn = post.findChildElement(LIKE_BTN_BY, ElementType.BUTTON);
         likeBtn.click();
     }
 
     public boolean isPostDisplayed(int postId) {
-        post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
-        postText = post.findChildElement(POST_TEXT_BY, ElementType.LABEL);
+        ILabel post = getElementFactory().getLabel(By.xpath(String.format(POST_XPATH, postId)), "Post");
+        ILabel postText = post.findChildElement(POST_TEXT_BY, ElementType.LABEL);
         return postText.state().isDisplayed();
     }
 }
